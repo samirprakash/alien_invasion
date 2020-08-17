7237c98f3afa8dfbc010bba62bef15b091235825
Samir Prakash <Samir.Prakash@gmail.com>
2020-08-16 22:07:33 -0500

    initial commit

---
+++ settings.py
@@ -0,0 +1,8 @@
+# Settings for alien invasion
+class Settings:
+    """ A class to store all settings for alien invasion """
+
+    def __init__(self):
+        self.screen_width = 1200
+        self.screen_height = 800
+        self.bg_color = (230, 230, 230)
