d222503f662746756355f3438b64b64302cea6a4
Samir Prakash <Samir.Prakash@gmail.com>
2020-08-30 22:37:16 -0500

    create a row of aliens

--- alien_invasion.py
+++ alien_invasion.py
@@ -51,6 +51,7 @@ class AlienInvasion:
                 self._check_keyup_events(event)
 
     def _check_keydown_event(self, event):
+        """ Evaluate key press events """
         if event.key == pygame.K_RIGHT:
             self.ship.moving_right = True
         elif event.key == pygame.K_LEFT:
@@ -61,17 +62,20 @@ class AlienInvasion:
             sys.exit()
 
     def _check_keyup_events(self, event):
+        """ Evaluate key unpressed """
         if event.key == pygame.K_RIGHT:
             self.ship.moving_right = False
         elif event.key == pygame.K_LEFT:
             self.ship.moving_left = False
 
     def _fire_bullet(self):
+        """ Fire bullets - max 3 at a time """
         if len(self.bullets) < self.settings.bullets_allowed:
             new_bullet = Bullet(self)
             self.bullets.add(new_bullet)
 
     def _update_bullets(self):
+        """ Move bullets toward the top and delete after exiting screen boundary """
         self.bullets.update()
 
         for bullet in self.bullets.copy():
@@ -79,8 +83,22 @@ class AlienInvasion:
                 self.bullets.remove(bullet)
 
     def _create_fleet(self):
+        """ Create fleet of aliens """
         alien = Alien(self)
-        self.aliens.add(alien)
+        alien_width = alien.rect.width
+
+        # find the number of aliens in a row
+        # spacing between each alien is equal to one alien width
+        available_space_x = self.settings.screen_width - (2 * alien_width)
+        number_aliens_x = available_space_x // (2 * alien_width)
+
+        # create a row of aliens
+        for alien_number in range(number_aliens_x):
+            alien = Alien(self)
+            # position an alien w.r.t the previous alien
+            alien.x = alien_width + 2 * alien_width * alien_number
+            alien.rect.x = alien.x
+            self.aliens.add(alien)
 
     def _update_screen(self):
         """Update images on the screen, and flip to the new screen."""
