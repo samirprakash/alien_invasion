d4700a6f196bcfb18b13d9879260ea2a98e31677
Samir Prakash <samir_prakash@apple.com>
2020-08-30 21:23:58 -0500

    draw bullets on spacebar event

--- alien_invasion.py
+++ alien_invasion.py
@@ -3,6 +3,7 @@ import sys
 
 import pygame
 
+from bullet import Bullet
 from settings import Settings
 from ship import Ship
 
@@ -25,6 +26,7 @@ class AlienInvasion:
 
         self.bg_color = self.settings.bg_color
         self.ship = Ship(self)
+        self.bullets = pygame.sprite.Group()
 
         pygame.display.set_caption("Alien Invasion")
 
@@ -33,6 +35,7 @@ class AlienInvasion:
         while True:
             self._check_events()
             self.ship.update()
+            self.bullets.update()
             self._update_screen()
 
     def _check_events(self):
@@ -52,6 +55,8 @@ class AlienInvasion:
         elif event.key == pygame.K_LEFT:
             # Move ship to the left
             self.ship.moving_left = True
+        elif event.key == pygame.K_SPACE:
+            self._fire_bullet()
         elif event.key == pygame.K_q:
             sys.exit()
 
@@ -63,12 +68,20 @@ class AlienInvasion:
             # Stop the ship from moving left
             self.ship.moving_left = False
 
+    def _fire_bullet(self):
+        new_bullet = Bullet(self)
+        self.bullets.add(new_bullet)
+
     def _update_screen(self):
         """Update images on the screen, and flip to the new screen."""
 
         # Redraw the screen during each pass through the loop
         self.screen.fill(self.bg_color)
         self.ship.blitme()
+
+        # draw bullets on screen when space is pressed
+        for bullet in self.bullets.sprites():
+            bullet.draw_bullet()
 
         # Make the most recently drawn screen visible
         pygame.display.flip()
--- settings.py
+++ settings.py
@@ -3,7 +3,16 @@ class Settings:
     """ A class to store all settings for alien invasion """
 
     def __init__(self):
+        # screen settings
         self.screen_width = 1200
         self.screen_height = 800
         self.bg_color = (230, 230, 230)
+
+        # ship settngs
         self.ship_speed = 1.5
+
+        # bullet settings
+        self.bullet_speed = 1.0
+        self.bullet_width = 3
+        self.bullet_height = 15
+        self.bullet_bg_color = (60, 60, 60)
